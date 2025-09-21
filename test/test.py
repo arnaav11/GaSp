73e23cbf28117a07bb9b7a7a34aeeabb57277967
gtaneja1 <gtaneja1@asu.edu>
2025-09-20 23:51:43 -0700

    imports

---
+++ test/test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler   #to split data into x and y x is numerical and y is target value????
+import os
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout  
